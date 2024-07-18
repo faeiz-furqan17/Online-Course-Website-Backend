bd5ba42771d8a4c7180f1bdf8e9d6668cc41aad4
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-07-18 19:57:42 +0500

    login and signup

--- freecs/base/urls.py
+++ freecs/base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
 
+from. import views
+
 urlpatterns = [
-  
- path("accounts/", include("django.contrib.auth.urls")),
+
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
 ]
